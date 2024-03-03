5f935eae08bdf1344e0ff72bff6d6d76297f925f
medomar-mansri <158220143+medomar-mansri@users.noreply.github.com>
2024-03-03 13:34:59 +0100

    Update pong.py

--- the game/pong.py
+++ the game/pong.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 
 
 def ball_animation():
@@ -8,7 +8,7 @@ def ball_animation():
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1 
     if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1   
+        ball_restart()
     if ball.colliderect(player)or ball.colliderect(opponent):
         ball_speed_x *= -1
 
@@ -19,6 +19,24 @@ def player_animation():
         player.top = 0 
     if player.bottom>= screen_height:
         player.bottom=screen_height
+
+def opponent_animation():
+    if opponent.top<ball.y:
+        opponent.top+=opponent_speed
+    if opponent.bottom>ball.y:
+        opponent.bottom-=opponent_speed
+    if opponent.top <=0:
+        opponent.top = 0 
+    if opponent.bottom>= screen_height:
+        opponent.bottom=screen_height
+
+def ball_restart():
+    global ball_speed_x, ball_speed_y
+    ball.center = (screen_width/2,screen_height/2)
+    ball_speed_y*=random.choice((1,-1))
+    ball_speed_x*=random.choice((1,-1))
+    
+
 
 
 
@@ -35,9 +53,10 @@ opponent = pygame.Rect(10,screen_height/2-70,10,140)
 light_grey=(200,200,200)
 bg_color = pygame.Color('grey12')
 
-ball_speed_x = 7
-ball_speed_y = 7 
+ball_speed_x = 7 * random.choice((1,-1))
+ball_speed_y = 7 * random.choice((1,-1))
 player_speed = 0
+opponent_speed = 7 
 
 
 
@@ -61,6 +80,8 @@ while True:
 
     ball_animation()
     player_animation()
+    opponent_animation()
+
     
 
     
